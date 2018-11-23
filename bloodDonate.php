a7a3df195b2b0ba749c5d97b859cd196e0c0f99a
Rohan Saxena <35458735+RhnSaxena@users.noreply.github.com>
2018-11-23 11:37:24 +0530

    Donate Blood + Updations
    Donate blood + Updation in other pages

---
+++ bloodDonate.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+include "./DB/DbConnection.php";
+$query='SELECT * FROM DONOR WHERE DONOR.dId="'.$_POST['dId'].'"';
+$result = mysqli_query($connection,$query);
+if(mysqli_num_rows($result)>0){
+	while ($row = mysqli_fetch_array($result)) {
+			if($row['dFname']==$_POST['dFname'] && $row['dLname']==$_POST['dLname']){
+				if($row['DBloodGroup']==$_POST['DBloodGroup']){
+					$sql='INSERT INTO blood VALUES ("'.$_POST['bId'].'","'.$_POST['bType'].'","'.$_POST['donationDate'].'","'.$_POST['dId'].'")';
+					echo $sql."<br><br>";
+					mysqli_query($connection,$sql);
+					echo "<br><br>";
+					echo mysqli_error($connection);
+				}
+				else echo "Blood Group doesn't matches";
+			}
+			else echo "Name entered doesn't matches with the donor's name";
+		}
+	}
+else{
+	echo"did doesn't exist";
+}
+
+?>
