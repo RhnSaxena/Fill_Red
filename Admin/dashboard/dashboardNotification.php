5845b68ad1be10ad42a492f7428923ec203e5415
thesid01 <sidkv.skv@gmail.com>
2018-11-26 23:23:29 +0530

    ofooo

--- Admin/dashboard/dashboardNotification.php
+++ Admin/dashboard/dashboardNotification.php
@@ -22,6 +22,7 @@
   <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet"> 
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
 
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
   <link rel="icon" type="image/png" sizes="96x96" href="">
 
 </head>
@@ -31,7 +32,10 @@
     <div class="row text-center">
       <h1 class="heading">Heading</h1>
     </div>
-
+    <div class="row text-center">
+      <h1 class="heading"><a href="dashboard.php"><i class="fas fa-home"></i></a>
+</h1>
+    </div>
     <div class="row">
       <div class="card">
         <div class="card-header">
