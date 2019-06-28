335444da35bc6651de389ca8473e1763f3e708d3
Rohan Saxena <35458735+RhnSaxena@users.noreply.github.com>
2019-06-28 15:20:54 +0530

    Update DbConnection.php

--- DB/DbConnection.php
+++ DB/DbConnection.php
@@ -5,13 +5,18 @@ $host = "localhost";
 
 //connection to the database
 //$dbhandle = mysql_connect($hostname, $username, $password)
-$connection = mysqli_connect('localhost', 'root', '','bloodbank');
+
+
+//I am commenting this for openshift, uncomment all comments followed by * 11,16,17
+// ** $connection = mysqli_connect('localhost', 'root', '','bloodbank');
  //or die(mysql_error());
 
 //select a database to work 
 $db = "bloodbank";
-$selected = mysqli_select_db($connection,"bloodbank") 
-  or die(mysqli_error($selected));
+// ** $selected = mysqli_select_db($connection,"bloodbank") 
+// **   or die(mysqli_error($selected));
+
+
 ?>
 
 
